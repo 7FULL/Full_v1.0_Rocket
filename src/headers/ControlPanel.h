d34ac3181d87d0f8e0385b23ca8cbe31e603024d
7FULL <phermidagomez@gmail.com>
2025-02-17 23:01:29 +0100

    DONE

--- src/headers/ControlPanel.h
+++ src/headers/ControlPanel.h
@@ -12,38 +12,41 @@ public:
 
     void begin();
 
-    bool isLaunchPlatformInitiated();
-    bool isWakeupInitiated();
-    bool isCountdownInitiated();
-    bool isLaunchStopped();
-    bool isEmergencyStop();
+    bool isLaunchPlatformInitiated() const;
+    bool isWakeupInitiated() const;
+    bool isCountdownInitiated() const;
+    bool isLaunchStopped() const;
+    bool isEmergencyStop() const;
 
     void updateDisplay(const char* message);
 
-    void update();
+    bool wakeUp();
+    bool countdown();
+    void startPlatform();
+    void emergencyStop();
+
+    void updateBuzzer();
 
 private:
-    // Pines de los botones
-    const uint8_t pinLaunchPlatform = 2; // INICIAR PLATAFORMA DE DESPEGUE
-    const uint8_t pinWakeup = 35;         // DESPERTAR AL ORDENADOR DE ABORDO
-    const uint8_t pinCountdown = 34;      // INICIAR CUENTA ATRAS
-    const uint8_t pinStopLaunch = 22;     // PARADA DEL LANZAMIENTO
-    const uint8_t pinEmergencyStop = 26;  // SETA DE EMERGENCIA
+    const uint8_t pinLaunchPlatform;
+    const uint8_t pinWakeup;
+    const uint8_t pinCountdown;
+    const uint8_t pinStopLaunch;
+    const uint8_t pinEmergencyStop;
 
-    const byte readingAddress[6] = "00001";
-    const byte writingAddress[6] = "00002";
-    const byte cePin = 4;
-    const byte csPin = 5;
+    const byte readingAddress[6];
+    const byte writingAddress[6];
+    const byte cePin;
+    const byte csPin;
 
-    // Display
+    const byte buzzerPin;
+
+    const byte countdownMax;
+
     LiquidCrystal_I2C lcd;
     RF24 radio; // CE, CSN
 
-    void startPlatform();
     void stopLaunch();
-    void emergencyStop();
-    void countdown();
-    void wakeUp();
 };
 
-#endif // CONTROLPANEL_H
+#endif
